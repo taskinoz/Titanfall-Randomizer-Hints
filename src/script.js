09d5d8a2e9f74d5bc8634c508d20b5f2de2beae5
Tristan Kennedy <tristan@taskinoz.com>
2021-09-05 17:24:52 +1000

    Made a fully functioning menu

--- src/script.js
+++ src/script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
   var active;
+  // When clicking one of the navigation buttons
   $('nav a').on('click', function(){
     href = $(this).attr('href');
-    $('container section').hide();
+    $('section.level').hide();
     if (active != null) {$(active).hide();}
     $(href).show();
     active = href;
-  })
+  });
+  // When clicking the show all button
+  $('#showall').on('click', function() {
+    $('section.level').show();
+    removeHash();
+  });
+
+  if (window.location.hash != "") {
+    $('section.level').hide();
+    $(window.location.hash).show();
+  }
+
+  function removeHash () {
+    history.pushState("", document.title, window.location.pathname + window.location.search);
+  }
 })
